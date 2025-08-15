e64683642623df399aee9a7ddd757807d0843abc
Victor-Hug0 <victorhugomourabarreto1@gmail.com>
2025-08-15 15:52:25 -0300

    feat: adding notNull validation in DTOs

--- src/main/java/br/com/victor/Marketplace/dto/CreateOrderPaymentRequestDTO.java
+++ src/main/java/br/com/victor/Marketplace/dto/CreateOrderPaymentRequestDTO.java
@@ -1,9 +1,12 @@
 package br.com.victor.Marketplace.dto;
 
 import br.com.victor.Marketplace.entity.enums.PayamentMethod;
+import jakarta.validation.constraints.NotNull;
 
 public record CreateOrderPaymentRequestDTO(
+        @NotNull(message = "Field payamentMethod can't be null!")
         PayamentMethod payamentMethod,
+        @NotNull(message = "Field installments can't be null!")
         Integer installments
 ) {
 }
--- src/main/java/br/com/victor/Marketplace/dto/ShippingAddressRequestDTO.java
+++ src/main/java/br/com/victor/Marketplace/dto/ShippingAddressRequestDTO.java
@@ -1,8 +1,13 @@
 package br.com.victor.Marketplace.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 public record ShippingAddressRequestDTO(
+        @NotNull(message = "Field existentAddressId can't be null!")
         Long existentAddressId,
+        @NotNull(message = "Field newAddress can't be null!")
         CreateAddressRequestDTO newAddress,
+        @NotNull(message = "Field saveNewAddressForFutureUse can't be null!")
         Boolean saveNewAddressForFutureUse
 ) {
 }
